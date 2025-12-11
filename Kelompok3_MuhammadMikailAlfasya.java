5c4de396a2063fda9b1a92edecd6132a5770971f
Mikail79 <barbarxpeople06@gmail.com>
2025-12-11 13:07:31 +0700

    add file mikail

---
+++ Kelompok3_MuhammadMikailAlfasya.java
@@ -0,0 +1,5 @@
+public class Kelompok3_MuhammadMikailAlfasya {
+    public static void main(String[] args) {
+        System.out.println("Muhammad Mikail Alfasya - 1324061");
+    }
+}
--- Mikail.java
+++
@@ -1,5 +0,0 @@
-public class Mikail {
-    public static void main(String[] args) {
-        System.out.println("Mikail - 1324061");
-    }
-}
