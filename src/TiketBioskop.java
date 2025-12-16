1c99372d3d5c7c6941bf9b0dd44bfc21ba0f12bc
Fathia Artha Utami <fathiaartha1109@gmail.com>
2025-12-16 11:34:56 +0700

    feat: menambahkan backend logic

--- src/TiketBioskop.java
+++ src/TiketBioskop.java
@@ -1,4 +1,14 @@
 package src;
 
-public class TiketBioskop {
+public class TiketBioskop extends Tiket {
+    
+    public TiketBioskop(String judulFilm, int harga) {
+        super(judulFilm, harga);
+    }
+
+    @Override
+    public int hitungTotalHarga(int jumlahTiket) {
+        // Logika bisnis sederhana
+        return this.hargaDasar * jumlahTiket;
+    }
 }
